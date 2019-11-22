2df59165e4248f1a6b77edee0c8b1f13e771399b
Mauricio <is716122@iteso.mx>
2019-11-22 01:00:04 -0600

    Se agrega script para dropdown del profe

---
+++ scripts/selectProfe.js
@@ -0,0 +1,31 @@
+let select = document.getElementById("selectProfe");
+
+let xhr = new XMLHttpRequest();
+
+// 2. Configurar: PUT actualizar archivo
+xhr.open('GET', "http://localhost:3000/profes");
+
+xhr.setRequestHeader('Content-Type', 'application/json');
+
+// 4. Enviar solicitud
+xhr.send();
+
+// 5. Una vez recibida la respuesta del servidor
+xhr.onload = function () {
+
+    if (xhr.status != 200) { // analizar el estatus de la respuesta HTTP
+        // alert("Usuario Error")
+        // Ocurrió un error
+        alert("Error. No se pueden mostrar los departamentos");
+
+
+    } else {
+        let dptos = JSON.parse(xhr.response);
+
+        dptos.forEach(element => {
+            select.innerHTML+= '<option value="'+element+'">'+element+' </option>';
+        });
+
+    }
+
+}
