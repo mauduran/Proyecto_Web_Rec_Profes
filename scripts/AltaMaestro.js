d16615f23080bf463f72b18e741cc5a20250d4a5
Anahí Santana <is715773@iteso.mx>
2019-11-21 18:52:30 -0600

    Script para AltaMaestro

---
+++ scripts/AltaMaestro.js
@@ -0,0 +1,7 @@
+let nombreM = document.getElementById("NombreMaestro"); 
+let departamento = document.getElementById("Departamento"); 
+let experiencia = document.getElementById("exp"); 
+let botonCancelar = document.getElementById("botonCancelar"); 
+let botonEnviar = document.getElementById("botonEnviar");
+
+botonEnviar.classList.add("disabled");
